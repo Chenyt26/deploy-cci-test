7ab257c099c75e4843db3cd936f04da27ac654f6
Chenyt26 <2396164919@qq.com>
2022-03-21 15:27:53 +0800

    Update install.ts

--- src/install.ts
+++ src/install.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as io from '@actions/io'
 import * as os from 'os'
-import * as exec from '@actions/exec';
+import * as cp from 'child_process'
 
 /*
  *
@@ -28,7 +28,12 @@ export async function installCciIamAuthenticatorByPlatform(platform: string): Pr
 
 export async function installCciIamAuthenticatorOnLinux(): Promise<void> {
     core.info('current system is Linux');
-    await exec.exec('curl', ['-LO', '"https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"']);
+    await (
+    cp.execSync(
+      `curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"`
+    ) || ''
+  ).toString()
+    
 }
 
 export async function installCciIamAuthenticatorOnMacos(): Promise<void> {
