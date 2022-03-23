c8fb8f40f605bfdcecb271f2709a807956c986e2
Chenyt26 <2396164919@qq.com>
2022-03-23 10:03:40 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 import * as context from './context';
 import * as install from './install';
 import * as auth from './auth';
+import * as image from './ImageUpdate';
 
 
 export async function run() {
@@ -12,8 +13,11 @@ export async function run() {
     // 安装cci-iam-authenticator
     const cciIamAuthPath = await install.downloadCciIamAuthenticator();
     
-    //配置iam的aksk
+    // 配置iam的aksk
     const cciIamAuth = await auth.configCciAuth();
+    
+    // 替换镜像地址
+    const imageConfi = await image.updateImage(input);
 }
 
 run();
