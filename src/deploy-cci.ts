612d2581289b734e88e44ee7696f39651d7de0c2
Chenyt26 <2396164919@qq.com>
2022-03-23 11:40:20 +0800

    Update deploy-cci.ts

--- src/deploy-cci.ts
+++ src/deploy-cci.ts
@@ -1,1 +1,10 @@
+import * as core from '@actions/core'
 
+export async function deployCCI(manifest: string): Promise<void> {
+  core.info('start deploy cci');
+  await (
+    cp.execSync(
+      `kubectl apply -f ${manifest}`
+    ) || ''
+  ).toString()
+}
