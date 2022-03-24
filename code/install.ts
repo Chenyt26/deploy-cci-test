33ffbc9029e3e0e58949b6c23175ebe7966b793f
Chenyt26 <2396164919@qq.com>
2022-03-24 11:25:47 +0800

    Update install.ts

--- code/install.ts
+++ code/install.ts
@@ -3,16 +3,14 @@ import * as io from '@actions/io'
 import * as os from 'os'
 import * as cp from 'child_process'
 
-/*
- *
- */
+
 export async function downloadCciIamAuthenticator(): Promise<void> {
     core.info('start install cci-iam-authenticator');
     let platform = os.platform();
     installCciIamAuthenticatorByPlatform(platform);
   }
 
-  /**
+/*
  * 针对不同操作系统完成cci-iam-authenticator安装
  * @param platform
  */
@@ -23,6 +21,9 @@ export async function installCciIamAuthenticatorByPlatform(platform: string): Pr
     
 }
 
+/*
+ * 目前cci-iam-authenticator只支持Linux和darwin
+ */
 export function getAuthDownloadURL(platform: string): string {
     switch (platform) {
         case 'Linux':
@@ -36,7 +37,6 @@ export function getAuthDownloadURL(platform: string): string {
 
 
 export async function installCciIamAuthenticator(downloadURL: string): Promise<void> {
-    core.info('current system is Linux');
     await (
     cp.execSync(
       `curl -LO "${downloadURL}"   && chmod +x ./cci-iam-authenticator && mv ./cci-iam-authenticator /usr/local/bin`
