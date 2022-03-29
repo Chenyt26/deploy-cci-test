c42fad66ba9fb9160d684acb158c3999219c23d2
Chenyt26 <2396164919@qq.com>
2022-03-29 15:50:44 +0800

    Update auth.ts

--- code/auth.ts
+++ code/auth.ts
@@ -8,10 +8,18 @@ import * as cp from 'child_process'
 export async function configCciAuth(): Promise<void> {
     core.info('Configuring IAM Authentication Information Using AK/SK');
     const input: context.Inputs = context.getInputs();
-    await (
+    const result = await (
     cp.execSync(
       `cci-iam-authenticator generate-kubeconfig --cci-endpoint=https://cci.${input.region}.myhuaweicloud.com --ak=${input.accessKey} --sk=${input.secretKey}`
     ) || ''
   ).toString()
+    core.info('generate-kubeconfig result: ' + result)
     
+    // 检查aksk 是否配置成功
+    const checkResult = await (
+    cp.execSync(
+      `kubectl config get-contexts`
+    ) || ''
+  ).toString()
+    core.info('generate-kubeconfig check result: ' + checkResult)
 }
